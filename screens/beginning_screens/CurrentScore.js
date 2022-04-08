2fcb3551754e159c3dd710f5d73aa104cd5b35d8
Seoyeon Kim <skim3328@gatech.edu>
2022-04-08 02:32:19 -0400

    Current Score Screen Completed

--- screens/beginning_screens/CurrentScore.js
+++ screens/beginning_screens/CurrentScore.js
@@ -1,17 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import React from "react";
 import { LAYOUT, COLORS } from "../../constants";
 
 const CurrentScore = () => {
   return (
-    <View style={{alignItems: "center"}}>
-      <Text style = {{fontSize: 80, textAlign: "center"}}>Current Score</Text>
-      <Text style = {{fontSize: 80, textAlign: "center", borderWidth: 2}}>00</Text>
-      <Text style = {{fontSize: 80, textAlign: "center", color: COLORS.primary_blue}}>Why am I doing this?</Text>
+    <View style={styles.mainContainer}>
+      <Text style = {{fontSize: 70, textAlign: "center"}}>Current Score</Text>
+      <Text style = {{fontSize: 50, textAlign: "center", borderWidth: 3.5, borderRadius: 10, borderColor: COLORS.primary_blue, marginTop: 20, paddingVertical: 20, paddingHorizontal: 100}}>00</Text>
+      <Text style = {{fontSize: 28, textAlign: "center", color: COLORS.primary_blue, marginTop: 20}}>Why am I doing this?</Text>
     </View>
   );
 };
 
 export default CurrentScore;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginTop: Dimensions.get("window").height * 0.2,
+    alignItems: "center",
+  },
+});
